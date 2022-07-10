c82400e0607fefa86e2786c93f9d6a0bba20012c
Gun1Yun <rjsdlf45@gmail.com>
2022-07-10 17:12:03 +0900

    [ADD] original model

---
+++ run.py
@@ -0,0 +1,174 @@
+import os
+import time
+import random
+import pickle
+import argparse
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from torch.utils.data import DataLoader
+
+from torchtext import datasets
+from torchtext.data.utils import get_tokenizer
+from torchtext.functional import truncate
+from torchtext.vocab import build_vocab_from_iterator
+
+from tqdm.auto import tqdm
+
+from models import OriginalModel
+
+## Argument parser ##
+parser = argparse.ArgumentParser()
+parser.add_argument("--task", type=str, choices=["original", "L2X"], default="original")
+parser.add_argument("--train", action="store_true")
+parser.set_defaults(train=False)
+
+## Set seed ##
+SEED = 1018
+random.seed(SEED)
+torch.manual_seed(SEED)
+
+## Hyperparameter ##
+max_len = 400
+vocab_size = 5000
+emb_size = 50
+batch_size = 128
+filter_size = 250
+kernel_size = 3
+hidden_size = 250
+n_epochs = 10
+learning_rate = 1e-1
+k = 10
+
+## MODEL PATH ##
+OG_MODEL_PATH = "./ckpt/original.pt"
+OG_DATA_PATH = "./data/"
+
+
+def load_data(vocab_size, tokenizer):
+    """
+    Args:
+        vocab_size: max size of vocabulary
+    """
+    train_dataset, test_dataset = datasets.IMDB(root=".data", split=("train", "test"))
+    train_iter = datasets.IMDB(split="train")
+
+    def yeild_tokens(data_iter):
+        for _, text in data_iter:
+            yield tokenizer(text)
+
+    vocab = build_vocab_from_iterator(
+        yeild_tokens(train_iter), specials=["<unk>"], max_tokens=vocab_size
+    )
+    vocab.set_default_index(vocab["<unk>"])
+
+    return train_dataset, test_dataset, vocab
+
+
+def text_pipeline(x):
+    return vocab(tokenizer(x))
+
+
+def label_pipeline(x):
+    return 0 if x == "neg" else 1
+
+
+def collate_batch(batch):
+    label_list, text_list, offsets = [], [], [0]
+    for (_label, _text) in batch:
+        label_list.append(label_pipeline(_label))
+        processed_text = torch.tensor(
+            truncate(text_pipeline(_text), max_seq_len=max_len), dtype=torch.int64
+        )
+        text_list.append(processed_text)
+        offsets.append(processed_text.size(0))
+    label_list = torch.tensor(label_list, dtype=torch.int64)
+    offsets = torch.tensor(offsets[:-1]).cumsum(dim=0)
+
+    text_list = nn.utils.rnn.pad_sequence(text_list, batch_first=True, padding_value=0)
+    return label_list, text_list, offsets
+
+
+def generate_original_prediction(train):
+    train_loader = DataLoader(
+        train_datasets, batch_size=batch_size, shuffle=False, collate_fn=collate_batch
+    )
+    val_loader = DataLoader(
+        val_datasets, batch_size=batch_size, shuffle=False, collate_fn=collate_batch
+    )
+
+    original_model = OriginalModel(vocab_size, emb_size, filter_size, kernel_size, hidden_size)
+    original_model.to(device)
+
+    if train:
+        criterion = nn.CrossEntropyLoss()
+        optimizer = optim.Adam(original_model.parameters(), lr=learning_rate)
+        # train
+        original_model.train()
+        for epoch in range(1, 1 + n_epochs):
+            print(f"Epoch [{epoch}/{n_epochs}] : ", end="")
+            total_acc, total_count, total_loss = 0, 0, 0
+            for _, (label, text, offsets) in tqdm(enumerate(train_loader)):
+                optimizer.zero_grad()
+                label, text, offsets = label.to(device), text.to(device), offsets.to(device)
+                predicted_label = original_model(text)
+                loss = criterion(predicted_label, label)
+                loss.backward()
+                optimizer.step()
+                total_acc += (predicted_label.argmax(1) == label).sum().item()
+                total_count += label.size(0)
+                total_loss += loss.item()
+            print(f"Acc : {total_acc/total_count}")
+
+        torch.save(original_model.state_dict(), OG_MODEL_PATH)
+        print(f"Train OG model is finished. saved OGmodel at {OG_MODEL_PATH}.")
+
+    original_model.load_state_dict(torch.load(OG_MODEL_PATH), strict=False)
+    original_model.eval()
+
+    with torch.no_grad():
+        train_preds = []
+        val_preds = []
+
+        # predicts train
+        for _, (label, text, offsets) in tqdm(enumerate(train_loader)):
+            label, text, offsets = label.to(device), text.to(device), offsets.to(device)
+            pred = original_model(text)
+            train_preds.append(pred)
+
+        # predicts validation
+        for _, (label, text, offsets) in tqdm(enumerate(val_loader)):
+            label, text, offsets = label.to(device), text.to(device), offsets.to(device)
+            pred = original_model(text)
+            val_preds.append(pred)
+
+        train_preds = torch.cat(train_preds)
+        val_preds = torch.cat(val_preds)
+
+        train_preds = train_preds.detach().cpu().numpy().tolist()
+        val_preds = val_preds.detach().cpu().numpy().tolist()
+
+        train_path = os.path.join(OG_DATA_PATH, "train_preds.pkl")
+        val_path = os.path.join(OG_DATA_PATH, "val_preds.pkl")
+
+        with open(train_path, "wb") as f_train, open(val_path, "wb") as f_val:
+            pickle.dump(train_preds, f_train)
+            pickle.dump(val_preds, f_val)
+
+        print(f"Generate OG prediction is finished. Saved predictions at {OG_DATA_PATH}")
+
+
+if __name__ == "__main__":
+    args = parser.parse_args()
+
+    device = torch.device("cuda")
+    tokenizer = get_tokenizer("basic_english")
+    train_datasets, val_datasets, vocab = load_data(vocab_size, tokenizer)
+
+    if args.task == "original":
+        generate_original_prediction(args.train)
+
+    elif args.task == "L2X":
+        pass
